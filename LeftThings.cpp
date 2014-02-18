c26ef3cb72d89e24d314fcbbc2c1f3aa95b2ddec
SergeyKouzmin <ksn270689@mail.ru>
2014-02-18 09:07:14 +0400

    Remove double files.

--- LeftThings.cpp
+++
@@ -1,9 +0,0 @@
-#include "LeftThings.h"
-
-LeftThings::LeftThings()
-	: isLeftThings_(false)
-{}
-
-void LeftThings::setRestTime(const boost::posix_time::ptime &restTime) {
-	restTime_ = restTime;
-}
